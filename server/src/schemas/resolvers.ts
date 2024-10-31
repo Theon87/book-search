cf2095f88ee61f591aefba44c24d36c6327fd5c4
theon87 <theonfrancis17@gmail.com>
2024-10-31 01:49:37 -0400

    add savebook and removebook to resolvers

--- server/src/schemas/resolvers.ts
+++ server/src/schemas/resolvers.ts
@@ -1,5 +1,6 @@
 import { signToken, AuthenticationError } from '../utils/auth.js';
 import User from '../models/User.js';
+// import { saveBook } from '../controllers/user-controller';
 
 interface User {
     username: string;
@@ -16,6 +17,25 @@ interface AddUserArgs {
     username: string;
     email: string;
     password: string;
+}
+
+interface saveBookArgs {
+    bookData: {
+        authors: string[];
+        description: string;
+        title: string;
+        bookId: string;
+        image: string;
+        link: string;
+    }
+}
+
+interface removeBookArgs {
+    bookId: string;
+}
+
+interface Context {
+    user?: User;
 }
 
 const resolvers = {
@@ -42,6 +62,26 @@ const resolvers = {
             const token = signToken(user.username, user.email, user.password);
             return { token, user };
         },  
+        saveBook: async (_parent: any, { bookData }: saveBookArgs, context: Context) => {
+            if (context.user) {
+                return await User.findOneAndUpdate(
+                    { _id: context.user.username },
+                    { $addToSet: { savedBooks: bookData } },
+                    { new: true, runValidators: true }
+                );
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        },
+        removeBook: async (_parent: any, { bookId }: removeBookArgs, context: Context):Promise<User | null> => {
+            if (context.user) {
+                return await User.findOneAndUpdate(
+                    { _id: context.user.username },
+                    { $pull: { savedBooks: { bookId } } },
+                    { new: true }
+                );
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        },
     },
 };
 
