470664299baeab2c007da21adce04f0e25b4e663
theon87 <theonfrancis17@gmail.com>
2024-10-31 01:21:02 -0400

    fix type errors in typedefs

--- server/src/schemas/typeDefs.ts
+++ server/src/schemas/typeDefs.ts
@@ -1,15 +1,49 @@
 const typeDefs = `
-  type Users {
+  type User {
     _id: ID
     username: String
     email: String
-    password: String
     bookCount: Int
+    savedBooks: [Book]
+  }
+    
+  type Book {
+    bookId: ID
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+  
+  input BookInput {
+    authors: [String]
+    description: String
+    title: String
+    bookId: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type me {
+    user: User
   }
 
   type Query {
     Users: [User]
     User(_id: ID!): User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
   }
 `;
 
