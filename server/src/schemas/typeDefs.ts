0a609c27e434190899b5e8b2951cf67d6e8202a1
theon87 <theonfrancis17@gmail.com>
2024-10-30 22:17:59 -0400

    add User typeDef

---
+++ server/src/schemas/typeDefs.ts
@@ -0,0 +1,12 @@
+const typeDefs = `
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    savedBooks: [Book]
+    bookCount: Int
+  }
+`;
+
+export default typeDefs;
