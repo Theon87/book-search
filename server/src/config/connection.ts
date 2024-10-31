b061342a1a6e3d00c5b41b910d8579120e91045a
theon87 <theonfrancis17@gmail.com>
2024-10-31 09:06:41 -0400

    add connection string to connection config

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -2,8 +2,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const MONGODB_URI = process.env.MONGODB_URI || '';
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks'); // This was the previous code.
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks';
 
 const db = async (): Promise<typeof mongoose.connection> => {
     try {
