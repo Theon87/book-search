eab6bc56adeba0bcb45f29de4569975546d5bff8
theon87 <theonfrancis17@gmail.com>
2024-10-31 01:22:41 -0400

    add addUser and login mutations

--- server/src/schemas/resolvers.ts
+++ server/src/schemas/resolvers.ts
@@ -1,7 +1,7 @@
+import { signToken, AuthenticationError } from '../utils/auth.js';
 import User from '../models/User.js';
 
 interface User {
-    _id: string;
     username: string;
     email: string;
     password: string;
@@ -12,11 +12,36 @@ interface userArgs {
     _id: string;
 }
 
+interface AddUserArgs {
+    username: string;
+    email: string;
+    password: string;
+}
+
 const resolvers = {
     Query: {
-        getSingleUser: async (_parent: any, { _id }: userArgs): Promise<User | null> => {
+        User: async (_parent: any, { _id }: userArgs): Promise<User | null> => {
             return await User.findOne({ _id });
         },
+    },
+    Mutation: {
+        addUser: async (_parent: any, { username, email, password }: AddUserArgs): Promise<{ token: string; user: User }> => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user.username, user.email, user._id);
+            return { token, user };
+        },
+        login: async (_parent: any, { email, password }: { email: string; password: string }): Promise<{ token: string; user: User }> => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('No user found with this email address');
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            const token = signToken(user.username, user.email, user.password);
+            return { token, user };
+        },  
     },
 };
 
