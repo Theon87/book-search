eaa90689ca736273f7d2d34980a695a4c1b1668b
theon87 <theonfrancis17@gmail.com>
2024-11-01 09:45:39 -0400

    updated fields in mutations

--- client/src/utils/mutations.ts
+++ client/src/utils/mutations.ts
@@ -1,16 +1,16 @@
 import { gql } from '@apollo/client';
 
 export const ADD_USER = gql`
-    mutation addUser($username: String!, $email: String!, $password: String!) {
-        addUser(username: $username, email: $email, password: $password) {
-            token
-            user {
-            _id
-            username
-            email
-            }
-        }
+mutation Mutation($username: String!, $email: String!, $password: String!) {
+  addUser(username: $username, email: $email, password: $password) {
+    token
+    user {
+      _id
+      username
+      email
     }
+  }
+}
 `;
 
 export const LOGIN = gql`
