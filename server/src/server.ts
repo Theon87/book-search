fd6e77efc81a7d1c84612b2774816e1270784993
theon87 <theonfrancis17@gmail.com>
2024-11-04 18:53:26 -0500

    define _dirname

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,6 +12,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 import routes from './routes/index.js';
 
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
