0b278a17275ba723b2f9f30fdf70bdc481a1c2fd
theon87 <theonfrancis17@gmail.com>
2024-11-06 09:14:22 -0500

    update dirname path

--- server/src/server.ts
+++ server/src/server.ts
@@ -44,7 +44,7 @@ const startApolloServer = async () => {
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
     app.use('*', (_req: Request, res: Response) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
     });
     app.use(routes);
   }
